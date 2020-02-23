32a611d597f2c4ea230430590f2429db0e9b6822
MrKampf <hello@mrkampf.com>
2020-02-23 10:39:12 +0100

    README.md update

--- src/proxmox/helper/connection.php
+++ src/proxmox/helper/connection.php
@@ -1,14 +1,37 @@
 <?php
-
-
+/**
+ * @copyright 2020 Daniel Engelschalk <hello@mrkampf.com>
+ */
 namespace proxmox\helper;
-
 
 use GuzzleHttp\Cookie\CookieJar;
 use proxmox\Exception\AuthenticationException;
 
 class connection
 {
+
+    /**
+     * Get CSRF token data from proxmox api for api auth
+     * @param $httpClient
+     * @param $url
+     * @param $username
+     * @param $password
+     * @param $authType
+     * @param $debug
+     * @return mixed
+     */
+    public static function getCSRFToken($httpClient,$url,$username,$password,$authType,$debug){
+        return $httpClient->post($url.'/api2/json/access/ticket', [
+            'verify' => false,
+            'debug' => $debug,
+            'form_params' => [
+                'username' => $username,
+                'password' => $password,
+                'realm' => $authType,
+            ],
+        ]);
+    }
+
 
     public static function getCookies($ticket,$hostname){
         return CookieJar::fromArray([
--- src/proxmox/pve.php
+++ src/proxmox/pve.php
@@ -1,54 +1,81 @@
 <?php
 /**
- * @copyright 2019 Daniel Engelschalk <hello@mrkampf.com>
+ * @copyright 2020 Daniel Engelschalk <hello@mrkampf.com>
  */
 namespace proxmox;
 
 use GuzzleHttp\Client;
 use proxmox\api\nodes;
 use proxmox\Exception\AuthenticationException;
+use proxmox\helper\connection;
 
+/**
+ * Class pve
+ * @package proxmox
+ */
 class pve
 {
-    private $hostname,$username,$password,$port,$authType,$httpClient,$debug,$CSRFPreventionToken,$ticket,$apiURL;
 
+    private $hostname, //Pormxox hostname
+        $username, //Proxmox username
+        $password, //Proxmox user password
+        $port, //Proxmox port
+        $authType, //Proxmox user type (pve or pam)
+        $httpClient, //The http client for connection to proxmox
+        $debug, //Want debug connection
+        $CSRFPreventionToken, //CSRF token for auth
+        $ticket, //Auth ticket
+        $apiURL; //API url
+
+    /**
+     * pve constructor.
+     * @param $param
+     * @param bool $debug
+     */
     public function __construct($param,$debug=false){
-        $this->hostname = $param['hostname'];
-        $this->username = $param['username'];
-        $this->password = $param['password'];
-        $this->port = $param['port'];
-        $this->authType = $param['authType'];
-        $this->httpClient = new Client();
-        $this->debug = $debug;
-        $this->apiURL = 'https://'.$this->hostname.':'.$this->port;
-        $json = json_decode($this->getCSRFToket(), true);
-        $this->setLoginTokens($json);
+        $this->hostname = $param['hostname']; //Save hostname in class variable
+        $this->username = $param['username']; //Save username in class variable
+        $this->password = $param['password']; //Save user password in class variable
+        $this->port = $param['port']; //Save port in class variable
+        $this->authType = $param['authType']; //Save auth type in class variable
+        $this->httpClient = new Client(); //Create new http client from GuzzleHttp
+        $this->debug = $debug; //Save the debug boolean variable
+        $this->apiURL = 'https://'.$this->hostname.':'.$this->port; //Create the basic api url
+        $json = json_decode($this->getCSRFToket(), true); //Get auth CSRF token
+        $this->setLoginTokens($json); //Set the login data for the proxmox api
     }
 
+    /**
+     * Set the login data for the proxmox api
+     * @param $json
+     * @return bool
+     */
     private function setLoginTokens($json){
-        if(!$json['data']){
-            $errorMSG = 'Can not login with this data.';
-            throw new AuthenticationException($errorMSG);
+        if(is_array($json)){ //Is $json are array
+            throw new AuthenticationException('Can\'t login with this data.');
         }
-        $this->CSRFPreventionToken = $json['data']['CSRFPreventionToken'];
-        $this->ticket = $json['data']['ticket'];
+        if(array_key_exists('data',$json)){//Is key 'data' in array
+            throw new AuthenticationException('Can\'t login with this data.');
+        }
+        $this->CSRFPreventionToken = $json['data']['CSRFPreventionToken']; //Save the CSRF token in class variable
+        $this->ticket = $json['data']['ticket']; //Save the ticket in class variable
+        return true; //Return true when function finish
     }
 
-
+    /**
+     * Get CSRF token data from proxmox api for api auth
+     * @return \Psr\Http\Message\StreamInterface | null
+     */
     private function getCSRFToket(){
-        $csrfRequest = $this->httpClient->post($this->apiURL.'/api2/json/access/ticket', [
-            'verify' => false,
-            'debug' => $this->debug,
-            'form_params' => [
-                'username' => $this->username,
-                'password' => $this->password,
-                'realm' => $this->authType,
-            ],
-        ]);
+        $csrfRequest = connection::getCSRFToken($this->httpClient,$this->apiURL,$this->username,$this->password,$this->authType,$this->debug); //Get CSRF token
+        if(!$csrfRequest){ //IF CSRF token variable empty/null
+            return null;
+        }
         return $csrfRequest->getBody();
     }
 
     /**
+     * Get nodes
      * @return nodes
      */
     public function nodes(){
